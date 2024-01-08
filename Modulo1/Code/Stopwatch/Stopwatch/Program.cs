036a4df447ba8f272a3a0625318cbd0834d3663b
maria_adelia <maria.adelia.contato@gmail.com>
2024-01-08 14:45:27 -0300

    finalizando o projeto de cronometro

--- Modulo1/Code/Stopwatch/Stopwatch/Program.cs
+++ Modulo1/Code/Stopwatch/Stopwatch/Program.cs
@@ -5,20 +5,56 @@ namespace Stopwatch
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            StartCountdown();
+            Menu();
         }
 
-        static void InputUser()
+        static void Menu()
         {
-            //Definir o valor que o usuário deseja colocar no stopwatch
+            Console.Clear();
+            Console.WriteLine("Stopwatch");
+            Console.WriteLine("Type the time you wish to count and at the end 's' for seconds or 'm' for minutes or 0 to exit");
+            string data = Console.ReadLine().ToLower();
+
+            if(data != "0")
+            {
+                var valueLength = data.Length;
+
+                int dataTime = int.Parse(data.Substring(0, valueLength - 1));
+                string dataSecondOrMinutes = data.Substring(valueLength - 1, 1);
+                Console.WriteLine(dataTime);
+                Console.WriteLine(dataSecondOrMinutes);
+
+                CheckingValue(dataTime);
+                
+                
+                StartCountdown(dataTime,dataSecondOrMinutes);
+
+            }
+            else
+            {
+                Console.WriteLine("Leaving the stopwatch");
+                System.Environment.Exit(0);
+            }
+            
+
+
         }
-        static void StartCountdown()
-        {
-            //Countdown baseado no input do usuário
-            int time = 10;
+
+        static void StartCountdown(int time, string secondsOrMinutes)
+        {            
+
+            if(secondsOrMinutes == "m")
+            {
+                time = time * 60;
+                Console.WriteLine(time);
+            }
+            
             int currentTime = 0;
+
+
 
             while (currentTime <= time)
             {
@@ -26,8 +62,36 @@ namespace Stopwatch
                 Console.WriteLine(currentTime);
                 Thread.Sleep(1000);
                 currentTime++;
-            }
+            }         
 
+            
+            Console.Clear();
+            Console.WriteLine("Stopwatch finished!");
+            Thread.Sleep(1000);
+            Menu();
+
+
+        }
+
+        static void CheckingValue(int value)
+        {
+
+            try
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Incorrect value");
+                    Thread.Sleep(1000);
+
+                    Menu();
+
+                }
+            }
+            catch(Exception e)
+            {                
+                throw new ArgumentException($"The {value} is not a valid number");
+            }
+            
         }
     }
 
--- Modulo1/Notebook/Segundo Projeto - Cronometro.md
+++ Modulo1/Notebook/Segundo Projeto - Cronometro.md
@@ -4,10 +4,12 @@
    -  Fazer um cronometro
    -  Como funciona um cronometro:
       -  Vou precisar perguntar para o usuário quanto tempo ele quer para a contagem (tipo quero que o cronometro conte 10 min, etc), para fazer a contagem do 0 ao valor. Esse tempo conta Horas, minutos, segundos
-      -  Vou precisar
--  s
 
 
-Assistir as aulas do cronômetro (por não ter ideia de como fazer) e depois fazer o exercício
+**Thread**
+- Thread.Sleep(*milissegundos desejados*) => para deixar o programa "dormindo", para que espere um determinado limite de tempo
+
+**Substring**
+- Variáveis tipo string tem o método *substring*, esse método permite pegar uma parte da cadeia de caracteres. Você passa primeiro o index desse caractere e pode passar também quantos caracteres você deseja
 
 ç
